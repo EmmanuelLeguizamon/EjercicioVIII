30bc6874432f6d89d267d28fd0528ced9c9a6ff6
EmmanuelLeguizamon <ema.legui.carp@gmail.com>
2021-09-05 16:09:27 -0300

    Ejercicio 8

--- EjercicioVIII/Program.cs
+++ EjercicioVIII/Program.cs
@@ -6,7 +6,33 @@ namespace EjercicioVIII
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
+            string f;
+
+            Console.WriteLine("Ingrese una frase. Si quiere terminar, escriba Fin.");
+
+            f = Console.ReadLine().ToLower();
+
+
+            if (f.Contains("fin".ToLower()) == true)
+            
+                {
+                    Console.WriteLine("La frase tiene la palabra Fin.");
+                }
+                
+               
+                else
+                {
+                Console.WriteLine("La frase no tiene la palabra Fin.");
+            }
+            Console.ReadKey();
+
         }
+
+
+
+
+
     }
+
 }
